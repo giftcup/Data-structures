9db80dcb3cf8dd02df1a943fcfd3fe997a91fc43
giftcup <salometambe@gmail.com>
2021-12-05 19:03:50 +0100

    Implementation of the bubble sort algorithm

--- sorts/bubble.c
+++ sorts/bubble.c
@@ -67,12 +67,12 @@ void print_list(int *list, int num) {
 void bubble_sort(int *list, int num) {
     int temp;
 
-    for (int i = 0; i < num; i++) {
-        for (int j = i+1; j < num; j++) {
-            if (list[i] > list[j]) {
-                temp = list[i];
-                list[i] = list[j];
-                list[j] = temp;
+    for (int i = 0; i < num-1; i++) {
+        for (int j = i+1; j < num-i-1; j++) {
+            if (list[j] > list[j+1]) {
+                temp = list[j];
+                list[j] = list[j+1];
+                list[j+1] = temp;
             }
         }
     }
